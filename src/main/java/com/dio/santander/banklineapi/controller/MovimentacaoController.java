91365fc260f3078e1b715394b4bac3f135ca83c1
Dhyego Luiz Damasco Pedroso <40304466+DhyegoPedroso@users.noreply.github.com>
2022-05-15 11:06:48 -0300

    Get movimentacao idConta

--- src/main/java/com/dio/santander/banklineapi/controller/MovimentacaoController.java
+++ src/main/java/com/dio/santander/banklineapi/controller/MovimentacaoController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,6 +28,11 @@ public class MovimentacaoController {
 	@GetMapping
 	public List<Movimentacao> findAll() {
 		return repository.findAll();
+	}
+	
+	@GetMapping("/{idConta}")
+	public List<Movimentacao> findAll(@PathVariable("idConta") Integer idConta){
+		return repository.findByIdConta(idConta);
 	}
 
 	@PostMapping
--- src/main/java/com/dio/santander/banklineapi/repository/MovimentacaoRepository.java
+++ src/main/java/com/dio/santander/banklineapi/repository/MovimentacaoRepository.java
@@ -1,9 +1,13 @@
 package com.dio.santander.banklineapi.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.dio.santander.banklineapi.model.Movimentacao;
 
 public interface MovimentacaoRepository extends JpaRepository<Movimentacao, Integer> {
 
+	public List<Movimentacao>findByIdConta(Integer idConta);
+	
 }
